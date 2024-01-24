7edd24ee37aa1c8cb06a2bc0df98c71dbb7c06c1
yukad2 <myuratq@gmail.com>
2024-01-25 02:45:56 +0900

    報酬上方修正

--- server/main.lua
+++ server/main.lua
@@ -1,7 +1,7 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local SafeCodes = {}
-local cashA = 250 --<<how much minimum you can get from a robbery
-local cashB = 450 --<< how much maximum you can get from a robbery
+local cashA = 4000 --<<how much minimum you can get from a robbery
+local cashB = 8000 --<< how much maximum you can get from a robbery
 
 CreateThread(function()
     while true do
