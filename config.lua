aa8325264ba88587b513dc73304128f92eb3fa7e
azutake <azusa_takemae@dwango.co.jp>
2024-01-27 12:45:33 +0900

    fix by yukad2

--- config.lua
+++ config.lua
@@ -1,11 +1,11 @@
 Config = {}
 Config.minEarn = 2000
-Config.maxEarn = 4000
+Config.maxEarn = 3000
 Config.RegisterEarnings = math.random(Config.minEarn, Config.maxEarn)
 Config.MinimumStoreRobberyPolice = 5
 Config.resetTime = (60 * 1000) * 45
 Config.tickInterval = 1000
-Config.stickyNoteChance = 50 -- Percent chance to get the safe code from a cash register
+Config.stickyNoteChance = 25 -- Percent chance to get the safe code from a cash register
 
 Config.Registers = {
     [1] = { vector3(-47.24, -1757.65, 29.53), robbed = false, time = 0, safeKey = 1, camId = 4 },
