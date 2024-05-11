4fe38bf2f35776bca1af7f1fa4aab2601d2178cb
azutake <azusa_takemae@dwango.co.jp>
2024-05-11 22:49:34 +0900

    fix font by yuka?

--- client/main.lua
+++ client/main.lua
@@ -196,7 +196,7 @@ end
 
 DrawText3Ds = function(coords, text)
     SetTextScale(0.35, 0.35)
-    SetTextFont(4)
+    SetTextFont(0)
     SetTextProportional(1)
     SetTextColour(255, 255, 255, 215)
     BeginTextCommandDisplayText('STRING')
